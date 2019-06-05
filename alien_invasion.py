bf38020b4dd603993393b8a5ce673d6b4392fadf
h1t3sh <hitesh.l33t@gmail.com>
2019-06-05 22:21:01 +0530

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,7 +6,7 @@ def run_game():
 	
     # Initialize game and create a screen object
     pygame.init()
-	ai_settings = Settings()
+    ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
