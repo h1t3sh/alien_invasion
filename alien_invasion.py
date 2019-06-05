8dbbb173f4f08e8a59cd7d08d4d7aea8e2abf535
h1t3sh <hitesh.l33t@gmail.com>
2019-06-05 22:19:23 +0530

    Update alien_invasion.py
    Introduced ai_settings object and made changes accordingly

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,15 @@
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
 	
     # Initialize game and create a screen object
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+	ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, \
+									  ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
-    # Set Background color
-    bg_color = (230,230,230)
 
     # Start the main loop for the game.
     while True:
@@ -18,7 +18,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
         # Redraw screen during each pass through loop
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 		
         # Make the most recently drawn screen visible
         pygame.display.flip()
